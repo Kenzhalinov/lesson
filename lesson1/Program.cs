82a9c7fcf3c8aca56e3a4ed5143bda9c1be55cd0
Maksat Kenzhalinov <mkenzhalinov@gmail.com>
2024-02-16 12:06:42 +0600

    Цепи данных

---
+++ lesson1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Console.WriteLine("Hello C#");
+
+/*
+Целый типы данных
+    int 
+    long 
+Вещественные типы данных 
+    float
+    double 
+Символьные типы данных 
+    char 
+Логические 
+    bool
+*/
+int value =100;
+double dobl = 5.55;
+char str = '!';
+bool flot = true;
+Console.WriteLine(value/2);
+Console.WriteLine(dobl);
+Console.WriteLine(str);
+Console.WriteLine(flot);
